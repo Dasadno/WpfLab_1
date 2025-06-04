b0383488d6df239f4708fe8fd24b0071be137a5d
Dasadno <Yakaleks96@gmail.com>
2025-06-04 11:40:11 +0500

    wa

--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -221,7 +221,7 @@ namespace WpfLab_1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/First_WPF_Practice;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/First_WPF_Practice;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
